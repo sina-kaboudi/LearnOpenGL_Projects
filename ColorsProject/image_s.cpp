58816d29e7ac4aacab640dd5c01df0637901d6bd
sina-kaboudi <kaboudi.sena@gmail.com>
2025-03-25 16:58:35 +0330

    Multiple lights added.

---
+++ ColorsProject/image_s.cpp
@@ -0,0 +1,2 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <FirstProject/stb_image.h>
--- Headers_and_Libraries/Includes/FirstProject/shader_s.h
+++ Headers_and_Libraries/Includes/FirstProject/shader_s.h
@@ -93,10 +93,19 @@ public:
     {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
-    void setMat4(const std::string& name, glm::mat4 matrix)
+    void setMat3(const std::string& name, glm::mat3 value)
     {
         unsigned int transformLoc = glGetUniformLocation(ID, name.c_str());
-        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(matrix));
+        glUniformMatrix3fv(transformLoc, 1, GL_FALSE, glm::value_ptr(value));
+    }
+    void setMat4(const std::string& name, glm::mat4 value)
+    {
+        unsigned int transformLoc = glGetUniformLocation(ID, name.c_str());
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(value));
+    }
+    void setVec3(const std::string& name, glm::vec3 value) const
+    {
+        glUniform3f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z);
     }
 
 private:
